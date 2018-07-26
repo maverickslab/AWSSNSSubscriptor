33216fc320ada8f0918805bb84c3a08e2597bec8
Mavericks's Dev <mavericks@mavericksss-macbook-air.local>
2018-07-26 10:37:42 -0400

    Initial Commit

---
+++ AWSSNSSubscriptor/AWSSNSSubscriptor.h
@@ -0,0 +1,19 @@
+//
+//  AWSSNSSubscriptor.h
+//  AWSSNSSubscriptor
+//
+//  Created by Mavericks's iOS Dev on 7/26/18.
+//  Copyright © 2018 Mavericks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AWSSNSSubscriptor.
+FOUNDATION_EXPORT double AWSSNSSubscriptorVersionNumber;
+
+//! Project version string for AWSSNSSubscriptor.
+FOUNDATION_EXPORT const unsigned char AWSSNSSubscriptorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AWSSNSSubscriptor/PublicHeader.h>
+
+
